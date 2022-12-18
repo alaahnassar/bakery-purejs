3d2b51702230e84aa76d177520b9ba7ed6c158b1
alaanassar <alaanassar1211@gmail.com>
2022-12-18 17:33:39 +0200

    active

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,5 @@
 
-
+//------------------counter--------------
 clickCounter();
 localStorage.clickcount = Number(localStorage.clickcount) - 1;
 document.getElementById("count").innerHTML = localStorage.clickcount;
@@ -15,58 +15,6 @@ function clickCounter() {
 let addTo = document.getElementsByClassName("add");
 for (let i = 0; i < addTo.length; i++) {
     addTo[i].addEventListener("click", clickCounter);
-}
-//----------filter----------
-var btnFilter = document.getElementsByClassName("btnFilter");
-
-var all = document.querySelectorAll(".filterDiv");
-
-var arr = [];
-all.forEach((item) => {
-    arr.push(item);
-});
-console.log(arr)
-var result;
-
-function filterSelection(product) {
-    result = arr.filter((item) => {
-        if (product == "all") {
-            item.style.display = "block";
-        } else if (product == "filterDiv") {
-            item.style.display = "none";
-            if (item.classList.contains("filterDiv")) {
-                item.style.display = "block";
-            }
-        } else if (product == "cake") {
-            item.style.display = "none";
-            if (item.classList.contains("cake")) {
-                item.style.display = "block";
-            }
-        }
-        else if (product == "donats") {
-            item.style.display = "none";
-            if (item.classList.contains("donats")) {
-                item.style.display = "block";
-            }
-        }
-        else if (product == "cupCakes") {
-            item.style.display = "none";
-            if (item.classList.contains("cupCakes")) {
-                item.style.display = "block";
-            }
-        }
-        else if (product == "Macarons") {
-            item.style.display = "none";
-            if (item.classList.contains("Macarons")) {
-                item.style.display = "block";
-            }
-        }
-    });
-}
-
-//--------------scroll-------------------------
-function scrollToTop() {
-    window.scrollTo(0, 0);
 }
 //--------------slider-------------------------
 var index = 0;
@@ -89,4 +37,66 @@ function showSlider(direction) {
         sliders[i].classList.remove('main');
     }
     sliders[index].classList.add('main');
+}
+//----------filter----------
+
+var all = document.querySelectorAll(".filterDiv");
+var arr = [];
+all.forEach((item) => { arr.push(item); });
+var result;
+function filterSelection(product) {
+    result = arr.filter((item) => {
+        if (product == "all") {
+            item.style.display = "block";
+        } else if (product == "filterDiv") {
+
+            item.style.display = "none";
+            if (item.classList.contains("filterDiv")) {
+                item.style.display = "block";
+            }
+        } else if (product == "cake") {
+
+
+            item.style.display = "none";
+            if (item.classList.contains("cake")) {
+                item.style.display = "block";
+
+            }
+        }
+        else if (product == "donats") {
+            item.style.display = "none";
+            if (item.classList.contains("donats")) {
+                item.style.display = "block";
+
+            }
+        }
+        else if (product == "cupCakes") {
+            item.style.display = "none";
+            if (item.classList.contains("cupCakes")) {
+                item.style.display = "block";
+
+            }
+        }
+        else if (product == "Macarons") {
+            item.style.display = "none";
+            if (item.classList.contains("Macarons")) {
+                item.style.display = "block";
+            }
+        }
+    });
+}
+//-------------- active ------------------
+let btnContainer = document.getElementById("myBtnContainer");
+let btnFilter = btnContainer.getElementsByClassName("btnFilter");
+for (var i = 0; i < btnFilter.length; i++) {
+    btnFilter[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace("active", "");
+        this.className += " active";
+    });
+}
+
+//--------------scroll-------------------------
+function scrollToTop() {
+    window.scrollTo(0, 0);
 }
